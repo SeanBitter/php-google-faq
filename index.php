4bd28bb3885590d40337a8055d5def010c460f34
SeanBitter <paolodig@hotmail.it>
2022-07-11 17:46:24 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+Riscrivere questa pagina del sito google
+https://policies.google.com/faq.
+Ci sono diverse domande con relative risposte.
+Gestire il “Database” e la visualizzazione di queste domande e risposte con PHP.
+*/
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Google Faq</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+</head>
+<body>
+  <div class="container my-5">
+    
+  </div>
+</body>
+</html>
